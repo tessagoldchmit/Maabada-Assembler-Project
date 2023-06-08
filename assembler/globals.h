c0a8e7d42ff916c28f7267fa3c11115b03f38f2e
tessag <tessagoldchmit@gmail.com>
2023-06-08 23:08:27 +0300

    (~) update globals.h - added new constant MAX_LINE_LENGTH=80

--- assembler/globals.h
+++ assembler/globals.h
@@ -1,11 +1,19 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
 /* Maximum file name length */
 #define MAX_FILE_NAME 80
 
 /* Total memory size */
 #define MEMORY_SIZE 1024
 
+/* Max line length */
+#define MAX_LINE_LENGTH 80
+
 /* Defines a boolean type (T/F) */
 typedef enum {
     FALSE,
     TRUE
 } bool;
+
+#endif /* GLOBALS_H */

a5c20a46a844957fe7cc02b5e3a269dab73f0938
tessag <tessagoldchmit@gmail.com>
2023-08-08 23:36:11 +0300

    updates

--- assembler/first_pass.c
+++ assembler/first_pass.c
@@ -6,10 +6,10 @@
 #include "ast.h"
 #include "temp.h"
 
-void decode_data(char* line, ast ast_line_info, int *dc, data_image *my_data_image) {
+void decode_data(char *line, ast ast_line_info, int *dc, data_image *my_data_image) {
     data_node *new_node;
     int word_length = 0;
-    int i=0;
+    int i = 0;
 
     /* handle directive */
     if (ast_line_info.ast_word.directive_word.directive_type == STRING_TYPE) {
@@ -28,77 +28,76 @@ void decode_data(char* line, ast ast_line_info, int *dc, data_image *my_data_ima
     add_data_node(my_data_image, new_node);
 }
 
-int analyze_operands(char* line, ast ast_line_info, int *ic, code_image *my_code_image) {
+int analyze_operands(char *line, ast ast_line_info, int *ic, code_image *my_code_image) {
     code_node *new_node;
     int word_length = 0;
     int new_operand_code = 0;
     int new_operand_target = 0;
     int new_operand_source = 0;
-    int L=0;
+    int L = 0;
 
-    if (check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_A){
+    if (check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_A) {
         new_operand_code = ast_line_info.ast_word.instruction_word.instruction_name;
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == NUMBER_OPERAND_TYPE)
-            if(ast_line_info.ast_word.instruction_word.instruction_name == MOV_TYPE ||
-               ast_line_info.ast_word.instruction_word.instruction_name == ADD_TYPE ||
-               ast_line_info.ast_word.instruction_word.instruction_name == SUB_TYPE ||
-               ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE){
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == NUMBER_OPERAND_TYPE)
+            if (ast_line_info.ast_word.instruction_word.instruction_name == MOV_TYPE ||
+                ast_line_info.ast_word.instruction_word.instruction_name == ADD_TYPE ||
+                ast_line_info.ast_word.instruction_word.instruction_name == SUB_TYPE ||
+                ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE) {
                 /* TODO error! */;
-            }
-            else {
+            } else {
                 new_operand_target = IMMEDIATE_ADDRESS;
             }
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == SYMBOL_OPERAND_TYPE)
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == SYMBOL_OPERAND_TYPE)
             new_operand_target = DIRECT_ADDRESS;
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == REGISTER_OPERAND_TYPE)
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.target_type == REGISTER_OPERAND_TYPE)
             new_operand_target = REGISTER_ADDRESS;
 
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == NUMBER_OPERAND_TYPE)
-            if(ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE) {
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == NUMBER_OPERAND_TYPE)
+            if (ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE) {
                 /* TODO error! */;
-            }
-            else {
+            } else {
                 new_operand_source = IMMEDIATE_ADDRESS;
             }
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == SYMBOL_OPERAND_TYPE)
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == SYMBOL_OPERAND_TYPE)
             new_operand_source = DIRECT_ADDRESS;
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == REGISTER_OPERAND_TYPE)
-            if(ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE) {
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_a.source_type == REGISTER_OPERAND_TYPE)
+            if (ast_line_info.ast_word.instruction_word.instruction_name == LEA_TYPE) {
                 /* TODO error! */;
-            }
-            else {
+            } else {
                 new_operand_source = REGISTER_ADDRESS;
             }
-        if(new_operand_target == REGISTER_ADDRESS && new_operand_source == REGISTER_ADDRESS){
-            new_node = create_code_node_registers(line, DEFAULT_WORD_LENGTH, ast_line_info, new_operand_code, new_operand_target, new_operand_source); /* because of two registers */
-            L=DEFAULT_WORD_LENGTH;
-        }
-        else{
-            new_node = create_code_node(line, GROUP_A_WORD_LENGTH, ast_line_info, new_operand_code, new_operand_target, new_operand_source); /* group a handles 3 words */
-            L=GROUP_A_WORD_LENGTH;
+        if (new_operand_target == REGISTER_ADDRESS && new_operand_source == REGISTER_ADDRESS) {
+            new_node = create_code_node_registers(line, DEFAULT_WORD_LENGTH, ast_line_info, new_operand_code,
+                                                  new_operand_target,
+                                                  new_operand_source); /* because of two registers */
+            L = DEFAULT_WORD_LENGTH;
+        } else {
+            new_node = create_code_node(line, GROUP_A_WORD_LENGTH, ast_line_info, new_operand_code, new_operand_target,
+                                        new_operand_source); /* group a handles 3 words */
+            L = GROUP_A_WORD_LENGTH;
         }
     }
-    if(check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_B){
+    if (check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_B) {
         new_operand_code = ast_line_info.ast_word.instruction_word.instruction_name;
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == NUMBER_OPERAND_TYPE)
-            if(ast_line_info.ast_word.instruction_word.instruction_name == PRN_TYPE) {
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == NUMBER_OPERAND_TYPE)
+            if (ast_line_info.ast_word.instruction_word.instruction_name == PRN_TYPE) {
                 new_operand_target = IMMEDIATE_ADDRESS;
-            }
-            else {
+            } else {
                 /* TODO error! */
             }
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == SYMBOL_OPERAND_TYPE)
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == SYMBOL_OPERAND_TYPE)
             new_operand_target = DIRECT_ADDRESS;
-        if(ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == REGISTER_OPERAND_TYPE)
+        if (ast_line_info.ast_word.instruction_word.instruction_union.group_b.target_type == REGISTER_OPERAND_TYPE)
             new_operand_target = REGISTER_ADDRESS;
 
-        new_node = create_code_node(line, DEFAULT_WORD_LENGTH, ast_line_info, new_operand_code, new_operand_target, NO_OPERAND);
-        L=DEFAULT_WORD_LENGTH;
+        new_node = create_code_node(line, DEFAULT_WORD_LENGTH, ast_line_info, new_operand_code, new_operand_target,
+                                    NO_OPERAND);
+        L = DEFAULT_WORD_LENGTH;
     }
-    if(check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_C){
+    if (check_group(ast_line_info.ast_word.instruction_word.instruction_name) == GROUP_C) {
         new_operand_code = ast_line_info.ast_word.instruction_word.instruction_name;
         new_node = create_code_node(line, GROUP_C_WORD_LENGTH, ast_line_info, new_operand_code, NO_OPERAND, NO_OPERAND);
-        L=GROUP_C_WORD_LENGTH;
+        L = GROUP_C_WORD_LENGTH;
     }
     add_code_node(my_code_image, new_node);
     return L;
@@ -106,27 +105,26 @@ int analyze_operands(char* line, ast ast_line_info, int *ic, code_image *my_code
 
 void update_data_dc(symbol_table *my_symbol_table, int *ic) {
     symbol_node *symbol_node_pointer = my_symbol_table->first;
-    while(symbol_node_pointer != my_symbol_table->last){
-        if(symbol_node_pointer->symbol_type == DATA)
-            symbol_node_pointer->decimal_address+=*ic;
+    while (symbol_node_pointer != my_symbol_table->last) {
+        if (symbol_node_pointer->symbol_type == DATA)
+            symbol_node_pointer->decimal_address += *ic;
 
-        symbol_node_pointer=symbol_node_pointer->next_symbol;
+        symbol_node_pointer = symbol_node_pointer->next_symbol;
     }
-    if(symbol_node_pointer->symbol_type == DATA)
-        symbol_node_pointer->decimal_address+=*ic;
+    if (symbol_node_pointer->symbol_type == DATA)
+        symbol_node_pointer->decimal_address += *ic;
 }
-
 
 
 bool first_pass_process(char *filename_with_am_suffix, int *ic, int *dc, data_image *my_data_image,
                         code_image *my_code_image, symbol_table *symbol_table) {
     FILE *am_file;
-    char line[MAX_LINE_LENGTH];
+    char line[MAX_LINE_LENGTH + 2];
     bool symbol_flag = FALSE;
     bool error_flag = FALSE;
     int L;
-    int line_number=1;
-
+    int line_number = 0;
+    size_t line_length;
     /* Open .am file */
     am_file = fopen(filename_with_am_suffix, "r");
     if (am_file == NULL) {
@@ -135,15 +133,27 @@ bool first_pass_process(char *filename_with_am_suffix, int *ic, int *dc, data_im
     }
 
     /* Process each line of the source file */
-    while (fgets(line, MAX_LINE_LENGTH+1, am_file)) { /* TODO define 256 or higher, else the long line error handle never occurs */
-        printf("\n------------------------------------------------------------------------------\n");
-        printf("%d. %s",line_number, line);
+    while (fgets(line, MAX_LINE_LENGTH + 2, am_file)) {
+        line_number++;
 
-        if (strlen(line) > MAX_LINE_LENGTH) {
-            printf("Error: Line %d is longer than %d characters.\n", line_number, MAX_LINE_LENGTH);
-            error_flag=TRUE;
+        line_length = strlen(line);
+        /* Remove trailing newline character if present */
+        if (line_length > 0 && line[line_length - 1] == '\n') {
+            line[line_length - 1] = '\0';
+            line_length--;
         }
-        else {
+        printf("\n------------------------------------------------------------------------------\n");
+        printf("%d. %s\n", line_number, line);
+
+        if (line_length > MAX_LINE_LENGTH) {
+            printf("Error: Line %d is longer than %d characters.\n", line_number, MAX_LINE_LENGTH);
+            error_flag = 1;
+
+            /* Skip the remaining characters in the overlong line */
+            int c;
+            while ((c = fgetc(am_file)) != '\n' && c != EOF) {
+            }
+        } else {
             ast ast_line_info = get_ast_line_info(line, line_number);
             print_ast(&ast_line_info);
             if (ast_line_info.ast_word_type != ERROR) {
@@ -176,14 +186,12 @@ bool first_pass_process(char *filename_with_am_suffix, int *ic, int *dc, data_im
                 error_flag = TRUE;
             }
         }
-        line_number++;
     }
     print_symbol_table(symbol_table);
     if (error_flag) {
         printf("## First pass encountered errors.\n");
         return FALSE;
-    }
-    else {
+    } else {
         printf("## First pass completed successfully.\n");
         return TRUE;
     }
---
+++ assembler/inputs/todos_qa.as
@@ -0,0 +1,32 @@
+; INVALID OPERAND (maayan)
+; todo: why it prints Illegal oper?
+MAIN: mov @     r5 ,LENGTH
+
+; INVALID REGISTER (maayan)
+; todo: why it prints Illegal regi?
+mov @r9 ,K
+
+; ARGUMENTS FOR DATA MUST BE INTEGERS (tessa)
+; todo: handle decimal numbers
+K: .data 7.5, 8, 9.5
+
+
+;todo: there is a memory leak here, understand why (maayan)
+;useless labels (warning)
+;USELESS1: .entry HOME1
+;USELESS2: .extern HOME2
+
+;duplicated symbols (maayan)
+;SYMBOL ALREADY EXISTS
+;    .extern DUPLICATE
+;    .entry DUPLICATE
+
+;using non exist label (known only after first pass!!!) (maayan)
+; todo check me in a working program
+; SYMBOL DOES NOT EXIST
+MAIN: mov @r3 ,NOTEXIST
+
+
+;todo: understand why this symbol is invalid (tessa)
+; valid ahusharmuta!
+    PLUS: .data +1, -1

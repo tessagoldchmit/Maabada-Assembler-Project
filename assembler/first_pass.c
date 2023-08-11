b44a229c8793d301befac96259a0cdac32c070d8
Maayan <mk062000@gmail.com>
2023-08-11 12:26:48 +0300

    tessa rejects

--- assembler/first_pass.c
+++ assembler/first_pass.c
@@ -189,9 +189,6 @@ bool first_pass_process(char *filename_with_am_suffix, int *ic, int *dc, data_im
                         }
                     } else if (ast_line_info.ast_word.directive_word.directive_type == EXTERN_TYPE ||
                                ast_line_info.ast_word.directive_word.directive_type == ENTRY_TYPE) {
-                        /* TODO talk with tessa */
-                        if(ast_line_info.ast_symbol[1]!=NULL) /* reaching index 1 instead of 0 because null terminated */
-                            printf("Warning! Useless symbol before entry/extern in line %d", ast_line_info.line_number);
                         if (ast_line_info.ast_word.directive_word.directive_type == EXTERN_TYPE) {
                             if(add_symbol(symbol_table, ast_line_info.ast_word.directive_word.directive_option.symbol, dc,
                                        EXTERNAL)==FALSE){
